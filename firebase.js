850d2cf1b1660ff1a095ef5b5ce1eb4664a2f56c
Bence Veres <166554090+BenceV95@users.noreply.github.com>
2024-09-09 08:57:26 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -5,12 +5,7 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDBEKtMXL4PnhoBLhcMAunUl0wR6NwiA1c",
-  authDomain: "favfoods-5d4e3.firebaseapp.com",
-  projectId: "favfoods-5d4e3",
-  storageBucket: "favfoods-5d4e3.appspot.com",
-  messagingSenderId: "604404989553",
-  appId: "1:604404989553:web:60974d70d11f9f2349a5d0"
+  apiKey: "not used just for demo"
 };
 
 // Initialize Firebase
